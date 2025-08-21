bb8d523ea4045c0cb59320d397fd8b2a84d06337
ayman elfadl <aymanelfadl16@gmail.com>
2025-08-21 16:20:54 +0100

    ok

---
+++ module_03/ex02/ClapTrap.hpp
@@ -0,0 +1,36 @@
+#pragma once 
+
+#include <iostream>
+#include <sstream>
+
+class ClapTrap
+{
+private:
+    std::string name;
+    int health;
+    int maxHealth; 
+    int energy;
+    int attackDamage;
+
+public:
+    ClapTrap(const std::string _name);
+    ~ClapTrap();
+
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+
+
+    std::string getName() const;
+    int getHealth() const;
+    int getEnergy() const;
+    int getDamage() const;
+    int getMaxHealth() const;
+    
+    void setName(const std::string& newName);
+    void setHealth(int newHealth);
+    void setEnergy(int newEnergy);
+    void setDamage(int newDamage);
+    void setMaxHealth(int max);
+    
+};
