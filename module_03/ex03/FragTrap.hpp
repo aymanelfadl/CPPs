3910ce8123fb043b4b55c7a864cdcf8e7cbf70d2
ayman elfadl <aymanelfadl16@gmail.com>
2025-08-21 16:39:08 +0100

    ok

---
+++ module_03/ex03/FragTrap.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap
+{
+public:
+    FragTrap(const std::string& _name);
+    ~FragTrap();
+
+    void attack(const std::string& target);
+    void highFivesGuys(void);
+
+};
+
+
