8794bed10ff692660dfda4ddeacb7c2387833fcc
ayman elfadl <aymanelfadl16@gmail.com>
2026-03-11 20:02:15 +0000

    ok

--- module_07/ex01/iter.h
+++ module_07/ex01/iter.h
@@ -2,13 +2,6 @@
 
 #include <iostream>
 
-// template<typename T>
-// void iter(T *arr, const size_t len, void (*f)(T))
-// {
-//     for (size_t i = 0; i < len; i++)
-//         f(arr[i]);
-// }
-
 template<typename T, typename F>
 void iter(T* arr, size_t len, F f)
 {
--- module_07/ex01/main.cpp
+++ module_07/ex01/main.cpp
@@ -2,17 +2,22 @@
 
 void printInt(const int& value)
 {
-    std::cout << value << std::endl;
+    std::cout << value << " ";
 }
 
 void printStr(const std::string& value)
 {
-    std::cout << value << std::endl;
+    std::cout << value << " ";
 }
 
 void increment(int& x)
 {
     x++;
+}
+
+void upper(std::string& str) {
+    for (size_t i = 0; i < str.length(); i++)
+        str[i] = std::toupper(str[i]);
 }
 
 int main()
@@ -21,7 +26,10 @@ int main()
     
     iter(list, sizeof(list)/sizeof(int), increment);
     iter(list, sizeof(list)/sizeof(int), printInt);
-
+    std::cout << std::endl;
     std::string strs[] = {"1337", "is", "the", "best"};
+    iter(strs, sizeof(strs)/sizeof(std::string), upper);
     iter(strs, sizeof(strs)/sizeof(std::string), printStr);
+    std::cout << std::endl;
+
 }
